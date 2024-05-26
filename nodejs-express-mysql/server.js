8f83ad5f640b9731d9c7ba046118e9fab070efd2
munisri539 <162156225+munisri539@users.noreply.github.com>
2024-05-26 23:22:46 +0530

    Add files via upload

--- nodejs-express-mysql/server.js
+++ nodejs-express-mysql/server.js
@@ -21,8 +21,8 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
-require("./app/routes/tutorial.routes.js")(app);
-
+//require("./app/routes/tutorial.routes.js")(app);
+require("./app/routes/areas.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
